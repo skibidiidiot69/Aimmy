cb2f9a9ec074cdefe0ba769ec56f2e80ce808e53
skibidiidiot69 <noelkw2009@gmail.com>
2025-06-01 01:12:23 +0200

    Update MouseManager.cs

--- Aimmy2/InputLogic/MouseManager.cs
+++ Aimmy2/InputLogic/MouseManager.cs
@@ -22,7 +22,7 @@ namespace InputLogic
         private const uint MOUSEEVENTF_MOVE = 0x0001;
         private static double previousX = 0;
         private static double previousY = 0;
-        public static double smoothingFactor = 0.5;
+        public static double smoothingFactor = 0.1;
         public static bool IsEMASmoothingEnabled = false;
 
         [DllImport("user32.dll")]
